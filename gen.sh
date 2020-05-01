79a732111caa73404ccd8c04d46c4b71d0f75e05
Iurii Panarin <tduble94@gmail.com>
2020-05-01 18:44:21 +0200

    feat(api): generate new api

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/server proto/proto/v1/server.proto;
-protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/storage proto/proto/v1/storage.proto;
-protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/agent proto/proto/v1/agent.proto;
+protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/monitoring proto/proto/v1/squzy_monitoring.proto;
+protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/storage proto/proto/v1/squzy_storage.proto;
+protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated/agent_server proto/proto/v1/squzy_agent_server.proto;
--- main.go
+++
@@ -1,6 +0,0 @@
-package squzy_generated
-
-import (
-	_ "github.com/squzy/squzy_generated/generated/storage/proto/v1"
-	_ "github.com/squzy/squzy_generated/generated/server/proto/v1"
-)
