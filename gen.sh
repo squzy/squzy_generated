f882fc9af7f5fad4187720135b1dd2998645ab14
Iurii Panarin <tduble94@gmail.com>
2022-01-15 22:43:17 +0100

    feat(api): fixed imports

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-protoc -I./proto -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc,request_context=true:./generated proto/proto/v1/*.proto;
+protoc -I./proto  --go_out=./generated proto/proto/v1/*.proto;
